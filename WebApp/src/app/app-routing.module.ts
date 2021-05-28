be1d8af1014fa7bf944063aadb3cdd61adbdcbed
ichetanya <>
2021-05-29 01:06:47 +0530

    components and service created

--- WebApp/src/app/app-routing.module.ts
+++ WebApp/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookCategoryComponent } from './book-category/book-category.component';
+import { BookListingComponent } from './book-listing/book-listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    pathMatch:'full',
+    redirectTo:'searchFlight'
+  },
+  {
+    path : 'category',
+    component: BookCategoryComponent
+  },
+  {
+    path : 'search',
+    component: BookListingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookCategoryComponent } from './book-category/book-category.component';
+import { BookListingComponent } from './book-listing/book-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookCategoryComponent,
+    BookListingComponent
   ],
   imports: [
     BrowserModule,
